20e7d3326d966e0a3eb909dec4c5878707e6f763
MathieuGery <mathieu.gery@epitech.eu>
2023-01-14 19:19:23 +0100

    Improve colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,21 +45,21 @@ export default function Home() {
             <div key="1" className="overflow-hidden rounded-lg bg-neutral-600 px-4 py-5 shadow sm:p-6">
               <dt className="truncate text-sm font-medium text-gray-800">Previous Bet</dt>
               <div className='grid grid-cols-2 gap-0'>
-                <CurrencyDollarIcon className="mt-1 h-9 w-9 text-red-500" aria-hidden="true" />
+                <CurrencyDollarIcon className="mt-1 h-9 w-9 text-yellow-500" aria-hidden="true" />
                 <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{data.previous_bet_value}</dd>
               </div>
             </div>
             <div key="2" className="overflow-hidden rounded-lg bg-neutral-600 px-4 py-5 shadow sm:p-6">
               <dt className="truncate text-sm font-medium text-gray-800">On previous color</dt>
               <div className='grid grid-cols-2 gap-0'>
-                <ForwardIcon className="mt-1 h-9 w-9 text-red-500" aria-hidden="true" />
-                <dd className="mt-1 -ml-2 text-3xl font-semibold tracking-tight text-gray-900">{data.colors?.at(-1)}</dd>
+                <ForwardIcon className="mt-1 h-9 w-9 text-yellow-500" aria-hidden="true" />
+                <dd className="mt-1 -ml-3 text-3xl font-semibold tracking-tight text-gray-900">{data.colors?.at(-1)}</dd>
               </div>
             </div>
             <div key="3" className="overflow-hidden rounded-lg bg-neutral-600 px-4 py-5 shadow sm:p-6">
               <dt className="truncate text-sm font-medium text-gray-800">Was bet N°</dt>
               <div className='grid grid-cols-2 gap-0'>
-                <CurrencyDollarIcon className="mt-1 h-9 w-9 text-red-500" aria-hidden="true" />
+                <CurrencyDollarIcon className="mt-1 h-9 w-9 text-yellow-500" aria-hidden="true" />
                 <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{data.count - 1}</dd>
               </div>
             </div>
@@ -69,26 +69,29 @@ export default function Home() {
             <div key="1" className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
               <dt className="truncate text-sm font-medium text-gray-800">Previous Bet</dt>
               <div className='grid grid-cols-2 gap-0'>
-                <CurrencyDollarIcon className="mt-1 h-9 w-9 text-red-500" aria-hidden="true" />
+                <CurrencyDollarIcon className="mt-1 h-9 w-9 text-yellow-500" aria-hidden="true" />
                 <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{data.bet_value}</dd>
               </div>
             </div>
             <div key="2" className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
               <dt className="truncate text-sm font-medium text-gray-800">On previous color</dt>
               <div className='grid grid-cols-2 gap-0'>
-                <ForwardIcon className="mt-1 h-9 w-9 text-red-500" aria-hidden="true" />
-                <dd className="mt-1 -ml-2 text-3xl font-semibold tracking-tight text-gray-900">{data.colors?.at(0)}</dd>
+                <ForwardIcon className="mt-1 h-9 w-9 text-yellow-500" aria-hidden="true" />
+                <dd className="mt-1 -ml-3 text-3xl font-semibold tracking-tight text-gray-900">{data.colors?.at(0)}</dd>
               </div>
             </div>
             <div key="3" className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
               <dt className="truncate text-sm font-medium text-gray-800">Was bet N°</dt>
               <div className='grid grid-cols-2 gap-0'>
-                <CurrencyDollarIcon className="mt-1 h-9 w-9 text-red-500" aria-hidden="true" />
+                <CurrencyDollarIcon className="mt-1 h-9 w-9 text-yellow-500" aria-hidden="true" />
                 <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{data.count}</dd>
               </div>
             </div>
           </dl>
-          <div className='flex items-center justify-center mt-10'>
+          <a className='flex items-center justify-center'>
+            next color: {data.colors[1]}
+          </a>
+          <div className='flex items-center justify-center mt-4'>
             <button
               onClick={() => calc(setData, true)}
               type="button"
