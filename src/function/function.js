66072c6179f0db1ddf5cddc94096e54207b0d802
MathieuGery <mathieu.gery@epitech.eu>
2023-01-16 10:36:55 +0100

    Add history

--- src/function/function.js
+++ src/function/function.js
@@ -7,7 +7,8 @@ export function resetGame(setData) {
 }
 
 export function setGame(setData) {
-    const init_data = {"count": 0, "bet_value": 1, "previous_bet_value": 0, "colors": ["RED", "BLACK", "RED", "RED", "BLACK", "BLACK"]}
+    const init_data = {"count": 0, "bet_value": 1, "previous_bet_value": 0, "colors": ["RED", "BLACK", "RED", "RED", "BLACK", "BLACK"], "history": []}
+
     if (typeof window !== "undefined") {
         console.log("Init")
         localStorage.setItem("data", JSON.stringify(init_data))
@@ -17,9 +18,10 @@ export function setGame(setData) {
 
 export function calc(setData, win) {
     var data = JSON.parse(localStorage.getItem("data"))
+
+    data.history.push(JSON.parse(localStorage.getItem("data")))
     data["count"] = data["count"] += 1
     data["previous_bet_value"] = data["bet_value"]
-    console.log("data", data)
 
     if (win) {
         data["bet_value"] = 1
@@ -27,7 +29,6 @@ export function calc(setData, win) {
         data["bet_value"] =  data["bet_value"] * 2
     }
     data.colors.push(data.colors.shift())
-
     localStorage.setItem("data", JSON.stringify(data))
     setData(data)
 }
